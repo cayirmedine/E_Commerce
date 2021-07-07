e0ce7088154e4e154f99f747910b9a2760fff632
Medine Çayır <htc.mdn.2010@gmail.com>
2021-07-07 14:34:11 +0300

    Validators added

--- e_commerce_app/routes/account.js
+++ e_commerce_app/routes/account.js
@@ -2,8 +2,34 @@ var express = require('express');
 const { addressModel, usersModel } = require('../db');
 var router = express.Router();
 
-router.post("/add-address", (req, res, next) => {
-  addressModel.create(req.body).then((result) => res.json({data: result}), (err) => res.send("An error occoured: " +err));
+const {check, validationResult} = require("express-validator");
+
+router.post("/add-address",
+check('phone')
+    .matches(/^[0-9]{10}$/)
+    .withMessage('Please enter a valid phone number')
+    .custom((value) => {
+      return usersModel.findOne({
+        where: {
+          phone: value
+        }
+      }).then((user) => {
+        if(user) {
+          return Promise.reject('Phone already in use');
+        }
+      })
+    })
+    ,(req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array().forEach((error) => res.send(error.msg))
+            });
+        }
+
+        addressModel.create(req.body).then((result) => res.json({data: result}), (err) => res.send("An error occoured: " +err));
 })
 
 router.get("/addresses/:userId", (req, res, next) => {
@@ -20,6 +46,26 @@ router.put("/password/:userId", (req, res, next) => {
     usersModel.update(
         {
             password: req.body.password
+        },
+        {
+            where: {id: req.params.userId},
+            returning: true
+        }
+    ).then(([rowsUpdate, updatedUsers]) => {
+        res.json({data: rowsUpdate, updatedUsers});
+    })
+    
+})
+
+router.put("/update-info/:userId", (req, res, next) => {
+
+    usersModel.update(
+        {
+            fullname: req.body.fullname,
+            email: req.body.email, 
+            phone: req.body.phone, 
+            birthdate: req.body.birthdate, 
+            gender: req.body.gender
         },
         {
             where: {id: req.params.userId},
--- e_commerce_app/routes/users.js
+++ e_commerce_app/routes/users.js
@@ -1,13 +1,74 @@
 var express = require('express');
 var router = express.Router();
 
-const { usersModel } = require('../db');
+const {check, validationResult} = require("express-validator");
 
-router.post('/sign-up', (req, res, next) => {
+const { usersModel } = require('../db');
+//old phone format: ^\+?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$
+router.post('/sign-up', 
+  check('phone')
+    .matches(/^[0-9]{10}$/)
+    .withMessage('Please enter a valid phone number')
+    .custom((value) => {
+      return usersModel.findOne({
+        where: {
+          phone: value
+        }
+      }).then((user) => {
+        if(user) {
+          return Promise.reject('Phone already in use');
+        }
+      })
+    }),
+  check('birthdate')
+    .matches(/^\d{1,2}\ \d{1,2}\ \d{4}$/)
+    .withMessage('Please enter date in true format(True Format: DD MM YYYY)'),
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value) => {
+      return usersModel.findOne({
+        where: {
+          email: value
+        }
+      }).then((user) => {
+        if(user) {
+          return Promise.reject('E-mail already in use');
+        }
+      })
+    }),
+  check('password')
+    .isLength({min: 6})
+    .withMessage('Password must be longer than 6 characters')
+,(req, res, next) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty()) {
+    return res.status(400).json({
+      //success: false,
+      //errors: errors.array().forEach((error) => res.send(error.msg))
+      errors: 'error',
+      data: error
+    });
+  }
+
   usersModel.create(req.body).then((result) => {res.json({data: result})}, (err) => console.log(err));
 });
 
-router.post('/sign-in', (req, res, next) => {
+router.post('/sign-in',
+  check('phone')
+  .matches(/^\+?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/)
+  .withMessage('Please enter a valid phone number')
+,(req, res, next) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array()
+    });
+  }
+
   usersModel.findOne({
     where: {
       phone: req.body.phone
